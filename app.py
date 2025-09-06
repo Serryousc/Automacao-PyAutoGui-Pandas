397261c623a37ed872a6f48999591d850080930a
SerryousC <abrao.k@hotmail.com>
2025-09-06 17:04:15 -0300

    Update app.py

--- app.py
+++ app.py
@@ -2,18 +2,16 @@ import pyautogui
 import pandas as pd 
 from time import sleep 
 
-arqui = pd.read_csv("AdNoturno.csv",sep=";", encoding='latin-1') # Horas Extras
-matriculas = arqui.drop(columns=['NOME','QUANTIDADE']).T #Lembra de Add, 'VALOR' caso seja necessario 
+arqui = pd.read_csv("AdNoturno.csv",sep=";", encoding='latin-1')  # Using pandas, read the .csv file/ Usando pandas para ler o arquivo CSV
+matriculas = arqui.drop(columns=['NOME','QUANTIDADE']).T          # Remember to add a columns call 'VALOR' for cases that the Prodata System is not able to calculate the current benefit
+                                                                  # Lembra de adicionar a coluna 'VALOR' para os casos que não é possivel para o sistema prodata calcular o valor do beneficio (ex:Horas extras)
 
+for matricula in matriculas: #For each matricula in matriculas /Para cada matricula em matriculas
+    matri1 = str(arqui.loc[matricula,'MATRICULA'])  #Transform matricula in a string /Pega a matricula e transforma em uma string
+    quant1 = str(arqui.loc[matricula,'QUANTIDADE']) #Transform quantidade in a string /Pega a quantidade e transforma em uma string
+    #valor1 = str(arqui.loc[matricula,'VALOR'])     #Transform valor in a string /Pega o valor e transforma em uma string
 
-for matricula in matriculas:
-                                                    #para cada matricula em matriculas:
-    matri1 = str(arqui.loc[matricula,'MATRICULA'])  #pega a matricula e transforma em uma string
-    quant1 = str(arqui.loc[matricula,'QUANTIDADE']) #pega a quantidade e transforma em uma string
-    #valor1 = str(arqui.loc[matricula,'VALOR'])      #pega o valor e transforma em uma string
-
-
-    def automacao(matricula,quantidade,):
+    def automacao(matricula,quantidade,): #sequencia of click to insert data to the prodata system/ Sequencia de clicks para inserir os dados no sistema prodata
         pyautogui.doubleClick(1350,335, duration=1.5) #click for Matricula
         pyautogui.press('del')
         pyautogui.write(matricula)
@@ -28,9 +26,6 @@ for matricula in matriculas:
 
         pyautogui.click(1363,607,duration=1.5) #click for Add
     automacao(matri1,quant1)
-
-
-
 
 
 # click for Matricula: 1350,335
